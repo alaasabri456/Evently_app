bd6b3ef0a939f04c56de37ca315c676a84a39576
alaa <alaasabri456@gmail.com>
2025-11-21 01:31:01 +0200

    search by title in favorite events

--- lib/features/main_layout/favourite/favourite_tab.dart
+++ lib/features/main_layout/favourite/favourite_tab.dart
@@ -8,9 +8,15 @@ import '../../../core/widgets/event_item.dart';
 import '../../../models/category_model.dart';
 import '../../../models/event_model.dart';
 
-class FavouriteTab extends StatelessWidget {
+class FavouriteTab extends StatefulWidget {
   const FavouriteTab({super.key});
 
+  @override
+  State<FavouriteTab> createState() => _FavouriteTabState();
+}
+
+class _FavouriteTabState extends State<FavouriteTab> {
+  String searchText = "";
   @override
   Widget build(BuildContext context) {
     AppLocalizations appLocalizations=AppLocalizations.of(context)!;
@@ -19,19 +25,28 @@ class FavouriteTab extends StatelessWidget {
         children: [   Padding(
       padding:REdgeInsets.symmetric(horizontal: 16.0),
       child: TextField(
+        onChanged: (value) {
+         searchText=value;
+         setState(() {
+
+         });
+        },
           decoration: InputDecoration(
               prefixIcon:Icon(Icons.search,color: ColorsManager.blue,),
         hintText:appLocalizations.search_for_event,
         hintStyle:TextStyle(fontSize: 14.sp, fontWeight: FontWeight.bold, color: ColorsManager.blue),
 
           ),
-      
+
         ),),
           FutureBuilder(future: FirebaseService.getFavouriteEvents(context),
               builder: (context, snapshot) {
                 if(snapshot.connectionState==ConnectionState.waiting)return Center(child: CircularProgressIndicator(),);
     if (snapshot.hasError)return Center(child: Text(snapshot.error.toString()));
     List<EventModel>favEvents=snapshot.data??[];
+    if (!searchText.isEmpty) {
+      favEvents=filterByEventTitle(searchText, favEvents);
+    }
     return   Expanded(child: ListView.builder(itemBuilder: (context, index) =>EventItem(event:favEvents[index],markAsFavorite: true,) ,itemCount: favEvents.length,
     )
     );
@@ -40,5 +55,10 @@ class FavouriteTab extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  List<EventModel> filterByEventTitle(String value,List<EventModel>events){
+    List<EventModel>  filtered=events.where((event)=>event.title.toLowerCase().contains(value.toLowerCase())).toList();
+    return filtered;
   }
 }
